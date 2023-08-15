a6ab1a0a08be944ae01eb7954b9b59ccf27ebfe3
root <+>
2023-08-15 11:55:34 -0600

    8.15.2023 12:12pm

--- script.js
+++ script.js
@@ -181,3 +181,51 @@ function getPetInfo() {
         })
     })    
 }
+
+
+
+fetch("http://127.0.0.1:3000/dogs")
+.then((r) => r.json())
+.then(data => {
+    console.log(data)
+    fillOutCert(data)
+
+})
+
+
+let listIdNames= ["petName", "birthday", "birthmonth", "birthyear", "breed", "gender", "parent"]
+
+function fillOutCert(data) {
+    listIdNames.forEach((id) => {
+        let element = document.getElementById(`${id}`)
+        element.textContent = data[`${id}`]
+    })
+} 
+
+fillOutCert()
+
+
+
+
+// const PDFDocument = require("pdfkit")
+// const fs = require("fs")
+
+// let doc = new PDFDocument
+
+// doc.pipe(fs.createWriteStream("certificate.pdf"))
+
+// doc.image("PuppyBdayCertTemplate.png", 0, 0, {width: 595.28, height: 841.89})
+
+// doc.fontSize(12)
+// doc.text(petname, 100, 100)
+
+// doc.fontSize(12)
+// doc.text(birthdate1, 200, 100)
+
+// doc.fontSize(12)
+// doc.text(birthdate1, 200, 150)
+
+// doc.end();
+
+
+
